1342feb71d56159936d8d2d14b51bcb492e3ad70
Renan Morita <renan.morita@etec.sp.gov.br>
2024-04-16 07:45:11 -0300

    ativdade de mobile(odeio ds pprt)

---
+++ aula 02/variaveis.js
@@ -0,0 +1,17 @@
+var numero = 2;
+let texto = "Hello";
+const oQueÉisso = "?";
+
+console.log('Tipo var : ', numero);
+
+numero = 13
+console.log(texto + ' o numero agora é ' + numero);
+
+texto = 69
+console.log(texto + numero);
+//contatenar com + dois valores numericos vai acabr somando 
+
+console.log(oQueÉisso);
+
+
+
---
+++ aula03/funcoes 1.js
@@ -0,0 +1,15 @@
+function soma () {
+    let n1 = 2
+    let n2 = 6
+
+    console.log (n1 + n2)
+}
+
+soma()
+
+function SomaComParametros (n1, n2) {
+    console.log(n1 + n2)
+}
+
+SomaComParametros(8, 10)
+SomaComParametros(50, 10)
